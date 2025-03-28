a633a07e047bd0cbfaed4bba2c553fa364da3db5
Santosh <sahosantosh117@gmail.com>
2025-03-28 12:03:07 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div", {}, [React.createElement("h1", {}, "Hello World!"),
+    React.createElement("p", {}, "This is a paragraph")
+]);
+console.log(heading);//retrun object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);//convert object to h1
+//React.createElement () is object that represents the DOM element. It takes three arguments: the type of element, the properties of the element, and the content of the element.
+
