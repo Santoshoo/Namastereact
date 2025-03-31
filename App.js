36aa826039e9bd50808b714514cb7c1f00567a34
Santosh <sahosantosh117@gmail.com>
2025-03-31 11:17:36 +0530

    commit

--- App.js
+++ App.js
@@ -2,11 +2,51 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement("div", {}, [React.createElement("h1", {}, "Hello World!"),
-    React.createElement("p", {}, "This is a paragraph")
-]);
-console.log(heading);//retrun object
+// const heading = React.createElement("h1", {
+//     id:"title",
+//     key:"h1"
+// },"Heading is parcel");
+// //create an h1 element with empty object as properties and "Heading is parcel" as content
+
+
+// const heading2 = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h2",
+//   },
+//   "Heading is parcel"
+// );
+
+// const container=React.createElement(
+//     "div",
+//     {
+//         id: "container",
+//         key: "div",
+//     },
+//     [heading,
+//     heading2]
+// )
+//react element:just a object
+const heading =(
+  <h1 id="h1">hhhh</h1>
+)
+
+//react component
+
+const Header=()=>{
+  return (
+    <div>
+      {heading}
+      <h1>This is react component</h1>
+      <h2>This is h2 tag</h2>
+    </div>
+  );
+};
+
+
+//console.log(container);//retrun object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);//convert object to h1
+root.render(<Header/>);//convert object to h1
 //React.createElement () is object that represents the DOM element. It takes three arguments: the type of element, the properties of the element, and the content of the element.
 
--- README.md
+++ README.md
@@ -16,9 +16,40 @@ package-lock.json-keep track exact version of dependencies installed
 -Image Optimization
 -bundling
 -file watching algo-written in c++
--consistent hashing
+-consistent hashing algo 
 -code splitting
 -differntial bundling
 -error handling
 -Tree shaking-remove unused code
 -different dev and producution bundles
+-minify
+
+
+we  have our package manager which takes care of our   transitive dependencies of our code
+-transitive dependencies
+
+-babel just a js package
+Babel is a JavaScript compiler that converts modern ES6+ code into older JavaScript (ES5) so that it works on all browsers.
+
+
+-html vs jsx
+
+
+
+-if we write component inside a component called composition  component
+
+search 
+componet composition in react
+reconcilition
+diff algo
+
+
+
+
+#functional component just a normal function that return peice jsx
+
+-Is jsx mandatory-no
+-Is ES6 mandantory-no
+
+
+
